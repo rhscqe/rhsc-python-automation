65afae45fc3838e7414087cb6da5894996850022
Dustin Tsang <dtsang@redhat.com>
2013-03-25 16:02:10 -0400

    version test

--- test.py
+++ test.py
@@ -158,6 +158,26 @@ class FixtureFactory:
     def create_volume(self,cluster,params):
         return Volume.create(cluster,params)
 
+class TestBase(unittest.TestCase):
+    def setUp(self):
+        self.api        = ApiFactory().get_api()
+
+    def tearDown(self):
+        self.api.disconnect()
+
+import re
+class ProductInfoTest(TestBase):
+    def test_product_info(self):
+        self.api.get_product_info()
+
+    def test_version(self):
+        version = self.api.get_product_info().get_version()
+        self.assertTrue(re.match(r'\d',str(version.get_major())), "major version could not be obtained")
+        self.assertTrue(re.match(r'\d',str(version.get_minor())), "major version could not be obtained")
+        self.assertTrue(re.match(r'\d',str(version.get_build())), "major version could not be obtained")
+        self.assertTrue(re.match(r'\d',str(version.get_revision())), "major version could not be obtained")
+
+    
 
 class TestVolume(unittest.TestCase):
     def setUp(self):
